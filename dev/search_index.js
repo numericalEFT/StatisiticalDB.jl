423885f833f87fd8f2d82f9ad1a9c223ce1fb813
Documenter.jl <documenter@juliadocs.github.io>
2022-11-04 18:35:36 +0000

    build based on 6d9243a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StatsDataBase","category":"page"},{"location":"#StatsDataBase","page":"Home","title":"StatsDataBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StatsDataBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StatsDataBase]","category":"page"}]
+}
